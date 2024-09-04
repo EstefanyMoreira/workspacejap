3f845a9b93f70beab027f28bf39ffda73294c41c
Sofia Osano Corbo <sofia-199906@hotmail.com>
2024-09-04 20:59:27 -0300

    Guardado de Id de los productos y redirección a product-info

--- js/products.js
+++ js/products.js
@@ -10,7 +10,7 @@ let productsArray = [];
         for (let i = 0; i < array.length; i++) { 
             let product = array[i];
             htmlContentToAppend += `
-            <div class="product-item">
+            <div class="product-item" data-product-id="` + product.id + `">
                 <div>
                     <img src="` + product.image + `" alt="product image" class="imgProduct">
                 </div>
@@ -28,7 +28,7 @@ let productsArray = [];
             `;
         }
         document.getElementById("products-container").innerHTML = htmlContentToAppend; 
-        }
+    }
 
 // Cargar datos y mostrar productos cuando la página se haya cargado
     document.addEventListener("DOMContentLoaded", function() {
@@ -36,6 +36,25 @@ let productsArray = [];
             if (resultObj.status === "ok") {
                 productsArray = resultObj.data.products; 
                 showProductList(productsArray);
+                selectedProducts();
             } 
         })
     });
+
+// Guarda en localStorage la ID del producto seleccionado y redirige a la página product-info.html
+    function selectedProducts() {
+        let productItem = document.querySelectorAll(".product-item");
+        
+        productItem.forEach(function(products) {
+            
+            products.addEventListener("click", function() {
+                let idP = products.getAttribute("data-product-id");  
+                
+                localStorage.setItem("productID", idP);
+                window.location.href = 'product-info.html';
+            });
+        });
+    }
+// En la línea 13 del código se agregó el atributo data-product-id. 
+// El prefijo data- permite crear atributos personalizados. 
+// Son útiles para almacenar datos que se puedan usar en JS sin interferir con la estructura HTML.
