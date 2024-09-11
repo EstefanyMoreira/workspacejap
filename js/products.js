9cb9feb6ec217eb6a83f504047d5e20f77f67981
estefanymoreira <estefanymoreira06@gmail.com>
2024-09-11 19:28:45 -0300

    estilo de products

--- js/products.js
+++ js/products.js
@@ -14,13 +14,22 @@ function showProductList(array) {
                 <img src="` + product.image + `" alt="product image" class="imgProduct">
             </div>
                 <div>
+                <div class="nameButton">
                     <h4>` + product.name + `</h4> 
+                    <button type="button" class="btnBlockRojo" id="btnBlockRojo" value="carrito">
+                    <i class="fa-regular fa-heart"></i></button>
+                    </div>
                     <p class="description">` + product.description + `</p>
                     <br>
                     <br> 
+                    <div class=bloqueGrande>
                     <p> Precio </p>
                     <p id="precio"> `+ product.currency + ` ` + product.cost +  `
                     <small>` + product.soldCount + ` vendidos </small> </p>
+                    <div class="cont_boton">
+                    <button type="button" class="btnRojo" value="comprar">Comprar</button>
+                </div>
+                    </div>
                 </div>
             </div>
             <hr>
